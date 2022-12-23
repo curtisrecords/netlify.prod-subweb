79407536b1f7418dd869178d3f9a047ca2e6cd3e
CurtisAsia <cncurtismusic@gmail.com>
2022-12-23 22:30:07 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 
 Vue.use(VueRouter);
 
@@ -15,6 +17,21 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+Sentry.init({
+  Vue,
+  dsn: "https://8543059a95e5420886767789ff62559c@o1074926.ingest.sentry.io/4504378730151936",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: ["localhost", "my-site-url.com", /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
 });
 
 export default router;
